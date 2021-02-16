98ecffb77cf4b5aa4ff8d45f29e3f4b117f0a240
GueddaSabrine <66629944+GueddaSabrine@users.noreply.github.com>
2021-02-16 10:42:19 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LesVariablesExo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 10;
+            Console.WriteLine("Le nombre 1 est " + number1);
+            int number2 = 5;
+            Console.WriteLine("Le nombre 2 est " + number2);
+
+            Console.WriteLine("Avec ajout du nombre 33 au nombre 1, le résultat est " + (33 + number1));
+
+            Console.WriteLine("Avec incrémentation pour le nombre 2 le résultat est : " + ++number2);
+
+            Console.WriteLine("Le resultat de la division avec les 2premiers nombres est " + (number1 / number2));
+
+
+        }
+    }
+}
