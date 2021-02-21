140ff47807491de8a0a88efd10afd04bf6dd3f56
GueddaSabrine <66629944+GueddaSabrine@users.noreply.github.com>
2021-02-21 12:24:32 +0100

    Vexo3

--- Program.cs
+++ Program.cs
@@ -6,17 +6,22 @@ namespace LesVariablesExo3
     {
         static void Main(string[] args)
         {
-            int number1 = 10;
-            Console.WriteLine("Le nombre 1 est " + number1);
-            int number2 = 5;
-            Console.WriteLine("Le nombre 2 est " + number2);
+            Console.WriteLine("Entrez un premier nombre : ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Entrez un second nombre  : ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            number2++;
+            float sum = number1 + 33;
+            float division;
+            division = sum/number2;
 
-            Console.WriteLine("Avec ajout du nombre 33 au nombre 1, le résultat est " + (33 + number1));
+            Console.WriteLine($"Aprés ajout +33 le nombre est {sum}");
+            Console.WriteLine($"Aprés incrémentation le nombre est {number2}");
+            Console.WriteLine("Le resultat de la division avec les 2premiers nombres est " + division);
 
-            Console.WriteLine("Avec incrémentation pour le nombre 2 le résultat est : " + ++number2);
-
-            Console.WriteLine("Le resultat de la division avec les 2premiers nombres est " + (number1 / number2));
-
+            //Créer deux variables de type entier. Via la console, demander à l’utilisateur de renseigner ces nombres à l’aide 
+            //de 2 phrases distinctes. Rajouter 33 au premier nombre, 
+            //et incrémenter de 1 le deuxième nombre. Effectuer la division du premier nombre par le deuxième nombre. Afficher le résultat dans la console, dans une phrase.
 
         }
     }
